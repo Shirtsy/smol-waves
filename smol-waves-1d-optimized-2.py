8c9715b8c1b385e1545474528bea0aadf88986c4
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2023-06-21 18:32:28 -0700

    Actually needed a copy in order to roll array

--- smol-waves-1d-optimized-2.py
+++ smol-waves-1d-optimized-2.py
@@ -24,7 +24,6 @@ def preallocate_arrays() -> dict:
     state = {
     "point_magnitudes_current"  : np.zeros(NUMBER_OF_POINTS),
     "point_magnitudes_previous" : np.zeros(NUMBER_OF_POINTS),
-    "point_magnitudes_new"      : np.zeros(NUMBER_OF_POINTS),
     "point_velocities"          : np.zeros(NUMBER_OF_POINTS),
     "point_accelerations"       : np.zeros(NUMBER_OF_POINTS),
     "rolled_array"              : np.zeros(NUMBER_OF_POINTS),
@@ -38,15 +37,21 @@ def preallocate_arrays() -> dict:
 
 # Calculates a new array with updated magnitudes when given an array at a timestep and the previous timestep
 def update_point_magnitudes(state: dict):
+    # Ensure relative_total is 0
+    state["relative_total"].fill(0)
+
     # Get current velocity of all points
     np.subtract(state["point_magnitudes_current"], state["point_magnitudes_previous"], out= state["point_velocities"])
     np.divide(state["point_velocities"], TIMESTEP_LENGTH, out= state["point_velocities"])
     np.multiply(state["point_velocities"], VELOCITY_MULTIPLIER, out= state["point_velocities"])
 
+    # Copy current magnitudes to previous, now that previous no longer needed
+    np.copyto(state["point_magnitudes_previous"], state["point_magnitudes_current"])
+
     # Get total relative magnitude of neighboring points
     for i in [-1,1]:
         np.copyto(state["rolled_array"], state["point_magnitudes_current"])
-        np.roll(state["rolled_array"], i)
+        state["rolled_array"] = np.roll(state["rolled_array"], shift= i)
         # Copy current values into ends of rolled array to return zero during the math for the end bits
         np.copyto(state["rolled_array"], state["point_magnitudes_current"], where= state["array_ends"])
         np.subtract(state["rolled_array"], state["point_magnitudes_current"], out= state["rolled_array"])
@@ -64,7 +69,6 @@ def update_point_magnitudes(state: dict):
 
     # Update magnitudes based on the delta
     np.add(state["point_magnitudes_current"], state["point_velocities"], out= state["point_magnitudes_current"])
-
 
 # Iterates the simulation by one timestep
 def iterate_timestep(state: dict):
@@ -114,7 +118,7 @@ def main():
         # Oscillate a specific point
         OSC_TIME = 360
         OSC_STRENGTH = 5.0
-        OSC_TARGET_INDEX = 100
+        OSC_TARGET_INDEX = 2
         if state["time_current"] <= OSC_TIME:
             osc_angle = (state["time_current"]) % 360
             state["point_magnitudes_current"][OSC_TARGET_INDEX] = math.sin(math.radians(osc_angle)) * OSC_STRENGTH
@@ -125,8 +129,7 @@ def main():
         # Freeze a specific point, in this case the one on the far end
         state["point_magnitudes_current"][NUMBER_OF_POINTS - 1] = 0
 
-        #print(point_magnitudes_current)
-        #plot_values(point_magnitudes_current)
+        #plot_values(state["point_magnitudes_current"])
         time.sleep(TIMESTEP_DURATION_SECONDS)
         iterate_timestep(state)
 
